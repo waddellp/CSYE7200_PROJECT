82a307cd218716f2b8283b54e60fb79db0a5cac9
Patrick Waddell <patrickwaddell@gmail.com>
2020-11-15 11:47:01 -0500

    Add more unit tests for parsing of DateTime

--- src/test/scala/edu/neu/coe/csye7200/proj/USGeoSurveySpec.scala
+++ src/test/scala/edu/neu/coe/csye7200/proj/USGeoSurveySpec.scala
@@ -15,10 +15,46 @@ import org.scalatest.{FlatSpec, Matchers}
 class USGeoSurveySpec extends FlatSpec with Matchers {
 
   behavior of "DateTime"
-  it should "work for params" in {
+  /**
+   * Test successful parsing of the UTC date/time
+   */
+  it should "work for valid UTC date/time" in {
     val x = DateTime("2020-10-31T22:10:35.880Z")
     x should matchPattern {
       case DateTime(2020, 10, 31, 22, 10, 35) =>
+    }
+  }
+  /**
+   * Test failures for parsing of the UTC date/time
+   */
+  it should "not work for invalid year" in {
+    assertThrows[Exception] { // Expect Exception
+      DateTime("3020-10-31T22:10:35.880Z")
+    }
+  }
+  it should "not work for invalid month" in {
+    assertThrows[Exception] { // Expect Exception
+      DateTime("2020-20-31T22:10:35.880Z")
+    }
+  }
+  it should "not work for invalid day" in {
+    assertThrows[Exception] { // Expect Exception
+      DateTime("2020-10-41T22:10:35.880Z")
+    }
+  }
+  it should "not work for invalid hour" in {
+    assertThrows[Exception] { // Expect Exception
+      DateTime("2020-10-31T32:10:35.880Z")
+    }
+  }
+  it should "not work for invalid minute" in {
+    assertThrows[Exception] { // Expect Exception
+      DateTime("2020-10-31T22:60:35.880Z")
+    }
+  }
+  it should "not work for invalid second" in {
+    assertThrows[Exception] { // Expect Exception
+      DateTime("2020-10-31T22:10:60.880Z")
     }
   }
 }
