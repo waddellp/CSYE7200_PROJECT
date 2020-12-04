d006c9d2b8595cb61a83b9647cec8f2c3c6f3c29
Patrick Waddell <patrickwaddell@gmail.com>
2020-12-03 23:16:29 -0500

    Improve speed of Historical Lookup query

--- app/controllers/LookupController.scala
+++ app/controllers/LookupController.scala
@@ -5,6 +5,7 @@ import java.util.{Calendar, GregorianCalendar}
 import akka.actor.ActorSystem
 import javax.inject._
 import model.edu.neu.coe.csye7200.proj.{DateTime, ForecasterUtil, Location}
+import org.apache.spark.FutureAction
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import play.api.mvc._
@@ -28,6 +29,8 @@ class LookupController @Inject()(cc: MessagesControllerComponents, actorSystem: 
   import model.edu.neu.coe.csye7200.proj.USGeoSurvey
 
   private val postUrl = routes.LookupController.lookupPost()
+  private val spark = SparkSession.builder().appName("HistoricalLookup").master("local[*]").getOrCreate()
+  private val sc = spark.sparkContext
 
   def lookup = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok(views.html.lookup(form, postUrl))
@@ -43,27 +46,21 @@ class LookupController @Inject()(cc: MessagesControllerComponents, actorSystem: 
    */
   def lookupPost = Action.async { implicit request =>
     val formData: Data = form.bindFromRequest.get
-    getFutureUSGS(formData, 20.second).map (u => Ok(views.html.lookupresult(u)))
+    val result = getFutureUSGS(formData)
+    result map (res => Ok(views.html.lookupresult(res)))
   }
 
-  private def getFutureUSGS(formData: Data, delayTime: FiniteDuration): Future[Seq[USGeoSurvey]] = {
-    val promise: Promise[Seq[USGeoSurvey]] = Promise[Seq[USGeoSurvey]]()
-    actorSystem.scheduler.scheduleOnce(delayTime) {
-      val cal = Calendar.getInstance()
-      cal.setTime(formData.startDate)
-      val startDate = DateTime(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH), 0, 0, 0 )
-      cal.setTime(formData.endDate)
-      val endDate = DateTime(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH), 23, 59, 59 )
-      val spark = SparkSession.builder().appName("HistoricalLookup").master("local[*]").getOrCreate()
-      val sc = spark.sparkContext
-      val data: RDD[USGeoSurvey] = ForecasterUtil.loadData(sc)
-      val q = ForecasterUtil.getEarthquakes(data)
-      val qr = ForecasterUtil.getDateRange(q, startDate, endDate)
-      val qrl = ForecasterUtil.getLocationArea(qr, Location(formData.latitude, formData.longitude, ""), formData.radius)
-      val qrls = ForecasterUtil.sortByMagnitude(qrl)
-      promise.success(qrls.collect().toSeq)
-      spark.close()
-    }(actorSystem.dispatcher) // run scheduled tasks using the actor system's dispatcher
-    promise.future
+  private def getFutureUSGS(formData: Data): FutureAction[Seq[USGeoSurvey]] = {
+    val cal = Calendar.getInstance()
+    cal.setTime(formData.startDate)
+    val startDate = DateTime(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH), 0, 0, 0 )
+    cal.setTime(formData.endDate)
+    val endDate = DateTime(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH), 23, 59, 59 )
+    val data: RDD[USGeoSurvey] = ForecasterUtil.loadData(sc)
+    val q = ForecasterUtil.getEarthquakes(data)
+    val qr = ForecasterUtil.getDateRange(q, startDate, endDate)
+    val qrl = ForecasterUtil.getLocationArea(qr, Location(formData.latitude, formData.longitude, ""), formData.radius)
+    val qrls = ForecasterUtil.sortByMagnitude(qrl)
+    qrls.collectAsync()
   }
 }
